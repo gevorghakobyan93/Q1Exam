e0a06c152ba6b5320719307fafbde9425d470df0
gevorghakobyan <gev.hakobyan93@gmail.com>
2024-03-05 21:31:34 +0400

    Add programm

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿class Exam
+{
+    static void Main()
+    {
+        string[] array = {"Hello", "2", "world", ":-)" };
+        string [] newArray = CreateNewArray(array, 3);
+        Console.WriteLine($"[{string.Join(",", newArray)}]");
+    }
+
+    static string[] CreateNewArray(string [] array, int limit)
+    {   
+        int size = 0;
+        int index = 0;
+
+        //Calculate size of array that will fit in limit
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i].Length <= limit){
+                size++;
+            }
+        }
+        //Create array with exact count that matches of size
+        string [] newArray = new string[size];
+        for (int j = 0; j < array.Length; j++)
+        {
+             if(array[j].Length <= limit){
+                newArray[index] = array[j];
+                index ++;
+            }
+        }
+        return newArray;
+    }
+}
--- obj/Debug/net8.0/Q1Exam.AssemblyInfo.cs
+++ obj/Debug/net8.0/Q1Exam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q1Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c68fc86c6d7bcad2ba3c5b1f0c87bdf7f7536a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da05dad8394ade546f42212620bbfb3af9dcb0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q1Exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q1Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
